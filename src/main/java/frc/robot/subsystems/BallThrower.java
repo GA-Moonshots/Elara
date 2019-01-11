79b7192af553137c12b0d727afdb98716c7c1d3f
Brian Davis <davisb@gilmour.org>
2019-01-11 14:31:47 -0500

    making and learning about commands & subsystems

--- src/main/java/frc/robot/subsystems/BallThrower.java
+++ src/main/java/frc/robot/subsystems/BallThrower.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -10,12 +10,11 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
- * An example subsystem.  You can replace me with your own Subsystem.
+ * Add your docs here.
  */
-public class Motors extends Subsystem {
+public class BallThrower extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
-  
 
   @Override
   public void initDefaultCommand() {
