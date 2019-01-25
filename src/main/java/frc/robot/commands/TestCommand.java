0c8ebc47454ef30bd1958e7a06719d2e19d76ecc
Brian Davis <davisb@gilmour.org>
2019-01-25 14:06:46 -0500

    flipped the caps
    i think Mr. A would frown upon me for having a lowercase command

--- src/main/java/frc/robot/commands/TestCommand.java
+++ src/main/java/frc/robot/commands/TestCommand.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -8,52 +8,37 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
-import edu.wpi.first.wpilibj.command.PIDCommand;
-import frc.robot.Robot;
-/**
- * An example command.  You can replace me with your own command.
- */
-public class OneEightyTurn extends Command {
 
-  private double target;
-
-  public OneEightyTurn() {
+public class TestCommand extends Command {
+  public TestCommand() {
     // Use requires() here to declare subsystem dependencies
-    requires(Robot.drivymcDriveDriverson);
+    // eg. requires(chassis);
   }
 
   // Called just before this Command runs the first time
   @Override
   protected void initialize() {
-    target = Robot.gyro.getAngle() + 180;
   }
 
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
-      Robot.drivymcDriveDriverson.drive.arcadeDrive(0, .5);
-      if(Math.abs(Robot.gyro.getAngle() - target) < 5){
-        end();
-      }
   }
 
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return Math.abs(Robot.gyro.getAngle() - target) < 5;
+    return false;
   }
 
   // Called once after isFinished returns true
   @Override
   protected void end() {
-    Robot.drivymcDriveDriverson.drive.arcadeDrive(0,0);
   }
 
   // Called when another command which requires one or more of the same
   // subsystems is scheduled to run
   @Override
   protected void interrupted() {
-    Robot.drivymcDriveDriverson.drive.arcadeDrive(0,0);
   }
-
 }
