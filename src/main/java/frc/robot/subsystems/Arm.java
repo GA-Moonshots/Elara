e7e1580364e1b0a5d0139ad5b93b85c4fdc1180d
Mollie <mollieedmondson@yahoo.com>
2019-02-07 16:29:15 -0500

    Added Arm

---
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -0,0 +1,47 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.Jaguar;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.RobotMap;
+
+/**
+ * The system based around the elevator lift motor
+ */
+public class Arm extends Subsystem {
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+  // here's a motor i'm declaring here, sorry Mr. A it's not in a command
+  public Jaguar elevatorMotor = new Jaguar(RobotMap.ELEVATORPORT);
+  // for the love of God start the elevator down
+  public Encoder sampleEncoder = new Encoder(RobotMap.ARM_ENC_A, RobotMap.ARM_ENC_B, false, Encoder.EncodingType.k4X);
+  
+  /**
+   * Tracks elevator's porition relative to two limit switches
+   */
+  public enum ArmPosition {
+    ABOVE, BETWEEN, BELOW
+  }
+
+  public ArmPosition position = ArmPosition.BETWEEN;
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+
+
+  public Arm(){
+    armEncoder.setDistancePerPulse(0);
+    //sampleInput = new DigitalInput(2);
+  }
+}
