18ade7e207a62b7759986e71566dbc682308f386
dadiletta <dan.adiletta@gmail.com>
2019-02-16 14:16:47 -0500

    notReallyPID on new arm command

--- src/main/java/frc/robot/commands/ArmMoveTo.java
+++ src/main/java/frc/robot/commands/ArmMoveTo.java
@@ -21,37 +21,24 @@ public class ArmMoveTo extends Command {
 
   private double target;
   private int check;
-  private int requestedEncoderValue;
+  private int requestedAngleChange;
 
   private Arm arm = Robot.arm;
 
-  public ArmMoveTo(int requestedRotation) {
+  public ArmMoveTo(int requestedAngleChange) {
     // Use requires() here to declare subsystem dependencies
     requires(Robot.arm);
-    this.requestedEncoderValue = requestedEncoderValue;
+    this.requestedAngleChange = requestedAngleChange;
 
   }
 
   // Called just before this Command runs the first time
   @Override
   protected void initialize() {
-    target = arm.armEncoder.get() + requestedEncoderValue;
+    target = arm.armEncoder.get() + (requestedAngleChange * 5);
     check = 0;
   }
 
-  /**
-   * Using target, requestedRotation
-   * Calculates error
-   * output is the opposite value of error
-   * 
-   * Formula:
-   * output = ((0.4 / 180) * error) +- minimum power
-   * Add minimum power if moving clockwise
-   * Subtract minimum power if moving counterclockwise
-   * This does work, plug a few values in if unsure, and solve on paper.
-   * 
-   * @return output of movement
-   */
   private double notReallyPID() {
     // NOTE: Negative return values will increase the gyro's value
     double MAX_POWER = 0.7; // cap the power 
@@ -62,39 +49,50 @@ public class ArmMoveTo extends Command {
     double error = target - arm.armEncoder.get();
 
     // determine the power output neutral of direction
-    double output = Math.abs(error / requestedEncoderValue) * MAX_POWER;
+    double output = Math.abs(error / (requestedAngleChange * 5)) * MAX_POWER;
     if(output < MIN_POWER) output = MIN_POWER;
     if(output > MAX_POWER) output = MAX_POWER;
 
     // are we there yet? this is to avoid ping-ponging
     // plus we never stop the method unless our output is zero
-    if(Math.abs(error) < RobotMap.ANGLE_TOLERANCE*5) check++;
+    if(Math.abs(error) < RobotMap.ANGLE_TOLERANCE * 5) check++;
     if(check > ENOUGH_CHECKS) return 0.0;
 
     // determine the direction
     // if I was trying to go a positive angle change from the start
-    if(requestedEncoderValue > 0){
-      if(error > 0) return -output; // move in a positive direction
-      else return output; // compensate for over-turning by going a negative direction
+    if(requestedAngleChange > 0){
+      if(error > 0) return output; // move in a positive direction
+      else return -output; // compensate for over-turning by going a negative direction
     }
     // if I was trying to go a negative angle from the start
     else{
-      if(error < 0) return output; // move in a negative direction as intended
-      else return -output; // compensate for over-turning by moving a positive direction
+      if(error < 0) return -output; // move in a negative direction as intended
+      else return output; // compensate for over-turning by moving a positive direction
     }
   }
+
+
+
+  // Called repeatedly when this Command is scheduled to run.
+  @Override
+  protected void execute() {
+    
+    Robot.arm.armMotor.set(notReallyPID());
+  }
+
+
 
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
     return Robot.arm.armMotor.get() == 0
-        && Math.abs(arm.armEncoder.get() - target) < RobotMap.ANGLE_TOLERANCE*5;
+        && Math.abs(arm.armEncoder.get() - target) < RobotMap.ANGLE_TOLERANCE * 5;
   }
 
   // Called once after isFinished returns true
   @Override
   protected void end() {
-    Robot.arm.armMotor.set(speed); = 0;
+    Robot.arm.armMotor.set(0.0);
     check = 0;
   }
 
@@ -102,6 +100,6 @@ public class ArmMoveTo extends Command {
   // subsystems is scheduled to run
   @Override
   protected void interrupted() {
-    Robot.drivymcDriveDriverson.drive.arcadeDrive(0, 0);
+    Robot.arm.armMotor.set(0.0);
   }
 }
